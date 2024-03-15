04763cd50e915b27a8dead4e68ea7a658daba25b
Kasib-hub <kasibzxc@gmail.com>
2024-03-14 21:40:10 -0400

    beginning of restructure to better design practices

---
+++ backend/src/main/java/com/ted/milanopizza/techtalk/Lambda.java
@@ -0,0 +1,7 @@
+package com.ted.milanopizza.techtalk;
+
+public class Lambda {
+    public static void main(String[] args) {
+        System.out.println("hello world, learn some lambdas");
+    }
+}
