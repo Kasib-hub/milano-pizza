a7cca1889a70bf87eb9f8b5115db95793f8f7655
Kasib <kasibabdullah8@gmail.com>
2024-03-17 07:51:48 -0400

    basic parameterized test

--- backend/src/test/java/com/ted/milanopizza/api/MilanoPizzaApplicationTests.java
+++ backend/src/test/java/com/ted/milanopizza/api/MilanoPizzaApplicationTests.java
@@ -1,13 +1,20 @@
 package com.ted.milanopizza.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MilanoPizzaApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@ParameterizedTest
+	@ValueSource(strings = {"hello", "world"})
+	void contextLoads(String input) {
+		assertThat(input).isEqualTo("hello");
 	}
 
 }
